b0dd067063e584929ca95afeea288814d5c71b3a
ss49919201 <sakashin10291029@gmail.com>
2024-04-07 08:34:00 +0900

    chore: プロジェクトのセットアップ

---
+++ prisma/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      email: "example.com",
+      tasks: {
+        create: {
+          status: "TODO",
+        },
+      },
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
